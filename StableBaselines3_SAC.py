41cd436f82bc940df9de65b1534c4f4121cf355d
apsankaran <91084049+apsankaran@users.noreply.github.com>
2022-10-06 22:28:09 -0500

    Update StableBaselines3_SAC.py

--- StableBaselines3_SAC.py
+++ StableBaselines3_SAC.py
@@ -5,19 +5,12 @@
 import gym
 import numpy as np
 
-from stable_baselines3.sac.policies import MlpPolicy
 from stable_baselines3 import SAC
 
+env = gym.make('Ant-v2')
 
-env = gym.make('Pendulum-v1')
-
-model = SAC(MlpPolicy, env, verbose=1)
+model = SAC("MlpPolicy", env, verbose=1)
 model.learn(total_timesteps=50000, log_interval=10)
-model.save("sac_pendulum")
-
-del model # remove to demonstrate saving and loading
-
-model = SAC.load("sac_pendulum")
 
 obs = env.reset()
 while True:
