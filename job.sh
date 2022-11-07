e3c97f98ec38f43035bddafe7406bb024cd68535
apsankaran <91084049+apsankaran@users.noreply.github.com>
2022-11-07 14:50:55 -0600

    Add files via upload

---
+++ job.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+set -e
+
+# 1. setup anaconda environment
+
+# replace env-name on the right hand side of this line with the name of your conda environment
+
+ENVNAME=research
+
+# if you need the environment directory to be named something other than the environment name, change this line
+
+ENVDIR=$ENVNAME
+
+# these lines handle setting up the environment; you shouldn't have to modify them
+
+export PATH
+
+mkdir $ENVDIR
+
+tar -xzf $ENVNAME.tar.gz -C $ENVDIR
+
+. $ENVDIR/bin/activate
+
+# make sure the script will use your Python installation,
+
+# and the working directory as its home location
+
+export PATH=$PWD/python/bin:$PATH
+
+export PYTHONPATH=$PWD/$ENVNAME
+
+export HOME=$PWD
+
+pip install mujoco gym
+
+pip install git+https://github.com/carlosluis/stable-baselines3@fix_tests
+
+# run your script
+
+python3 StableBaselines3_SAC.py
